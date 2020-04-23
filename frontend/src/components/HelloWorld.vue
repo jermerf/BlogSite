29bffd21d01fe571f1a8e7e183ddc17ecaa17b03
Jermaine <jermerf@gmail.com>
2020-04-23 07:36:35 -0300

    Initial commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">    
+    <form method="POST" action="/auth/register">
+      <input type="text" name="username">
+      <input type="password" name="password">
+      <input type="submit" value="Register" />
+    </form>
+    <hr>
+    <form method="POST" action="/auth/login">
+      <input type="text" name="username">
+      <input type="password" name="password">
+      <input type="submit" value="Login" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
